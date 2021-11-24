fcf9c45830e1b807d6b9d6dbd3ea488ea228cbcf
r-lantern <67883249+r-lantern@users.noreply.github.com>
2021-11-24 17:44:32 -0500

    Adds product comparison and sets Twitter API tokens as environment variables

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from typing import List
 from src import config as config
+from src import consts as consts
 from src import mynintendo as mynintendo
 from src.twitter import Twitter
 from src.database import Database
@@ -10,6 +10,29 @@ def main():
     new_products = mynintendo.get_rewards(req)
 
     nintendoDB = Database(config.DB_URI, config.DB_NINTENDO)
+
+    product_deltas = {
+        consts.STATUS_NEW: [],
+        consts.STATUS_STOCK_IN: [],
+        consts.STATUS_STOCK_OUT: [],
+    }
+
+    for product in new_products:
+        old_product = nintendoDB.db[config.COLLECTION_REWARDS].find_one(
+            {"id": product["id"]}
+        )
+
+        if not old_product:
+            product_deltas[consts.STATUS_NEW].append(product)
+
+        elif "stock" in old_product:
+            if product["stock"]["available"] != old_product["stock"]["available"]:
+                if product["stock"]["available"] is True:
+                    product_deltas[consts.STATUS_STOCK_IN].append(product)
+                else:
+                    product_deltas[consts.STATUS_STOCK_OUT].append(product)
+
+    nintendoDB.override_collection(config.COLLECTION_REWARDS, new_products)
 
     twitter = Twitter()
 
--- src/consts.py
+++ src/consts.py
@@ -1,1 +1,11 @@
 PAGE_REWARDS_STORE = "https://my.nintendo.com/reward_categories/nintendo_store"
+
+STATUS_NEW = "new"
+STATUS_STOCK_IN = "stock_in"
+STATUS_STOCK_OUT = "stock_out"
+
+TAGS = {
+    STATUS_NEW: "NEW",
+    STATUS_STOCK_IN: "BACK IN STOCK",
+    STATUS_STOCK_OUT: "OUT OF STOCK",
+}
